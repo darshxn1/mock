30645d93636689a3d23b24ed1d4248fcd913ae00
darshxn1 <129256556+darshxn1@users.noreply.github.com>
2023-04-27 18:40:25 +0530

    Update settings.py

--- mockproject-backend/backend/settings.py
+++ mockproject-backend/backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware’
+    'whitenoise.middleware.WhiteNoiseMiddleware’,
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
